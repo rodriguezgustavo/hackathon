55e0d58587990997a4bd33550d9f59c31f5fb30a
Eric Kohan <eric.kohan@mercadolibre.com>
2015-10-16 20:40:41 -0300

    - refa app key

--- src/main/java/com/ml/hackathon/services/Geocoding.java
+++ src/main/java/com/ml/hackathon/services/Geocoding.java
@@ -3,8 +3,8 @@ package com.ml.hackathon.services;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
+import com.ml.hackathon.config.Config;
 import com.ml.hackathon.domain.LatLon;
-import com.ml.hackathon.meli_sdk.MeliException;
 
 /**
  * Created by ekohan on 10/16/15.
@@ -12,7 +12,7 @@ import com.ml.hackathon.meli_sdk.MeliException;
 public class Geocoding {
 
     public static LatLon geocode (String address) throws Exception {
-        GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyBxnZUMXifTzPulvFznNA2ctzT-dN6P4PA");
+        GeoApiContext context = new GeoApiContext().setApiKey(Config.GEOCODING_KEY);
 
         try {
             GeocodingResult[] results = GeocodingApi.newRequest(context)
