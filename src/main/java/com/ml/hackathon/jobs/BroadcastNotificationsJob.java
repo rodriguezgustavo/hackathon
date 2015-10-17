f304f2b920391c8c415bcefc974f4122682e98d6
Eric Kohan <eric.kohan@mercadolibre.com>
2015-10-17 01:35:08 -0300

    - add source and destination to invitation message

--- src/main/java/com/ml/hackathon/jobs/BroadcastNotificationsJob.java
+++ src/main/java/com/ml/hackathon/jobs/BroadcastNotificationsJob.java
@@ -4,10 +4,8 @@ import com.ml.hackathon.algorithms.Pricer;
 import com.ml.hackathon.algorithms.Scorer;
 import com.ml.hackathon.db.OrderDao;
 import com.ml.hackathon.db.ShippersDao;
-import com.ml.hackathon.domain.Order;
-import com.ml.hackathon.domain.OrderStatus;
-import com.ml.hackathon.domain.Shipper;
-import com.ml.hackathon.domain.ShipperScore;
+import com.ml.hackathon.domain.*;
+import com.ml.hackathon.services.Geocoding;
 import com.ml.hackathon.services.NotificationsSender;
 import org.apache.log4j.Logger;
 
@@ -35,13 +33,20 @@ public class BroadcastNotificationsJob implements Runnable {
                     List<Shipper> shippers = ShippersDao.getShippers();
                     List<ShipperScore> shippersScores = Scorer.getShippersForOrder(order, shippers);
 
+                    LatLon from = Geocoding.geocode(order.getSellerAddress());
+                    LatLon to   = Geocoding.geocode(order.getReceiverAddress());
+
+                    String fromJson = "{\"latitude\":" + from.getLatitude() + ", \"longitude\":" + from.getLongitude() + "}";
+                    String toJson = "{\"latitude\":" + to.getLatitude() + ", \"longitude\":" + to.getLongitude() + "}";
+
+
                     for(ShipperScore shipperScore : shippersScores) {
                         Map<String, String> data = new HashMap<String, String>();
                         data.put("order_id", order.getSellerAddress());
                         data.put("address_from", order.getSellerAddress());
                         data.put("address_to", order.getReceiverAddress());
-                        data.put("geo_from", "");
-                        data.put("geo_to", "");
+                        data.put("geo_from", fromJson);
+                        data.put("geo_to", toJson);
                         data.put("shipping_price",order.getShippingPrice().toString());
 
                         NotificationsSender.send(shipperScore.getShipper().getToken(), data);
